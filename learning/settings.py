1911de5aeed02ae0c8050dbe597cb82829d7d4f2
stonycat <zhuqiancg@163.com>
2017-08-08 14:19:53 +0800

    test in OSX

--- learning/settings.py
+++ learning/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'learning.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'VADB',   
+        'NAME': 'zqdata',   
         'USER': 'root',
         'PASSWORD': ' ',    
         'HOST': '127.0.0.1',
