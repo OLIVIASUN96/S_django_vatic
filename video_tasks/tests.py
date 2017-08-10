501d29506a2bc273481b7f4ae42a9dcb30840d83
stonycat <zhuqiancg@163.com>
2017-08-10 09:39:48 +0800

    must develop on ubuntu in py3

--- video_tasks/tests.py
+++ video_tasks/tests.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+# import unittest
+# from django.test import Client
+
+# class SimpleTest(unittest.TestCase):
+#     def test_details(self):
+#         client = Client()
+#         response = client.get('/customer/details/')
+#         self.assertEqual(response.status_code, 200)
+
+#     def test_index(self):
+#         client = Client()
+#         response = client.get('/customer/index/')
+#         self.assertEqual(response.status_code, 200)
